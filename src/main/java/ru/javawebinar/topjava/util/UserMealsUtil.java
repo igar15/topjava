a3697a411567ff85340d924a3af39aa08251ef19
igar15 <igar15@yandex.ru>
2021-01-28 11:56:40 +0300

    Realize 'filteredByCyclesInOnePass' method

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -28,6 +28,8 @@ public class UserMealsUtil {
         mealsTo.forEach(System.out::println);
 
         System.out.println(filteredByStreams(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
+
+        System.out.println(filteredByCyclesInOnePass(meals, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000));
     }
 
     public static List<UserMealWithExcess> filteredByCycles(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
@@ -56,6 +58,71 @@ public class UserMealsUtil {
                 .filter(userMeal -> TimeUtil.isBetweenHalfOpen(userMeal.getTime(), startTime, endTime))
                 .map(userMeal -> new UserMealWithExcess(userMeal, caloriesByDay.get(userMeal.getDate()) > caloriesPerDay))
                 .collect(Collectors.toList());
+    }
+
+    public static List<UserMealWithExcess> filteredByCyclesInOnePass(List<UserMeal> meals, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
+
+        List<UserMeal> mealsCopy = new ArrayList<>(meals);
+
+        Comparator<UserMeal> timeIntervalComparator = (userMeal1, userMeal2) -> {
+            if (!userMeal1.getDate().equals(userMeal2.getDate())) {
+                return userMeal1.getDateTime().compareTo(userMeal2.getDateTime());
+            }
+            boolean isUserMeal1InInterval = TimeUtil.isBetweenHalfOpen(userMeal1.getTime(), startTime, endTime);
+            boolean isUserMeal2InInterval = TimeUtil.isBetweenHalfOpen(userMeal2.getTime(), startTime, endTime);
+
+            if (!isUserMeal1InInterval && !isUserMeal2InInterval) {
+                return userMeal1.getDateTime().compareTo(userMeal2.getDateTime());
+            } else if (isUserMeal1InInterval && isUserMeal2InInterval) {
+                return userMeal1.getDateTime().compareTo(userMeal2.getDateTime());
+            } else if (isUserMeal1InInterval && !isUserMeal2InInterval) {
+                return 1;
+            } else if (!isUserMeal1InInterval && isUserMeal2InInterval) {
+                return -1;
+            }
+            return 0;
+        };
+
+        mealsCopy.sort(timeIntervalComparator);
+
+        List<UserMealWithExcess> filteredUserMealsWithExcess = new ArrayList<>();
+        LocalDate tempLocalDate = mealsCopy.get(0).getDate();
+        int tempCaloriesCounter = 0;
+        for (int i = 0; i < mealsCopy.size(); i++) {
+            UserMeal userMeal = mealsCopy.get(i);
+            if (!userMeal.getDate().equals(tempLocalDate)) {
+                int tempListIndex = i - 1;
+                while (true) {
+                    UserMeal tempUserMeal = mealsCopy.get(tempListIndex);
+                    if (TimeUtil.isBetweenHalfOpen(tempUserMeal.getTime(), startTime, endTime)) {
+                        filteredUserMealsWithExcess.add(new UserMealWithExcess(tempUserMeal, tempCaloriesCounter > caloriesPerDay));
+                        tempListIndex--;
+                    }
+                    else {
+                        tempLocalDate = userMeal.getDate();
+                        tempCaloriesCounter = userMeal.getCalories();
+                        break;
+                    }
+                }
+            } else if (i == mealsCopy.size() - 1) {
+                int tempListIndex = i;
+                while (true) {
+                    UserMeal tempUserMeal = mealsCopy.get(tempListIndex);
+                    tempCaloriesCounter += tempUserMeal.getCalories();
+                    if (TimeUtil.isBetweenHalfOpen(tempUserMeal.getTime(), startTime, endTime)) {
+                        filteredUserMealsWithExcess.add(new UserMealWithExcess(tempUserMeal, tempCaloriesCounter > caloriesPerDay));
+                        tempListIndex--;
+                    }
+                    else {
+                        break;
+                    }
+                }
+            } else {
+                tempCaloriesCounter += userMeal.getCalories();
+            }
+        }
+
+        return filteredUserMealsWithExcess;
     }
 
 }
