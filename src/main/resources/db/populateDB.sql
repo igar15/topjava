bd7eaf555c37ea9f5ef3b3edca45e0b2bb78323b
igar15 <igar15@yandex.ru>
2021-02-20 18:19:52 +0300

    Realize meals initDB, populateDB sql scripts

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,12 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (date_time, description, calories, user_id)
+VALUES ('2020-01-30 10:00', 'USER breakfast', 500, 100000),
+       ('2020-01-30 13:00', 'USER lunch', 1000, 100000),
+       ('2020-01-30 20:00', 'USER dinner', 500, 100000),
+       ('2020-01-31 00:00', 'ADMIN boundary meal', 100, 100001),
+       ('2020-01-31 10:00', 'ADMIN breakfast', 1000, 100001),
+       ('2020-01-31 13:00', 'ADMIN lunch', 500, 100001),
+       ('2020-01-31 20:00', 'ADMIN dinner', 410, 100001);
